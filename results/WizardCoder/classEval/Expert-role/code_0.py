37ba1f9d1c27faf360b297a400b80c456764edab
Marcus Paulsson <mapau990@student.liu.se>
2025-03-27 22:55:39 +0100

    new tests with different local models

--- results/WizardCoder/classEval/Expert-role/code_0.py
+++ results/WizardCoder/classEval/Expert-role/code_0.py
@@ -1,20 +1,48 @@
-# Prompt user for age and gender input
-age = int(input("Please enter your age: "))
-gender = input("Please enter your gender (M/F): ")
-
-if gender == 'M':
-    if age < 18:
-        print("You must wash for at least 20 seconds before and after using the toilet or touching any surface.")
-    elif age >= 65:
-        print("Please dry your hands with soap before continuing with any activity")
-    else:
-        print("Handwashing is not required for you.")
-elif gender == 'F':
-    if age < 18:
-        print("You must wash for at least 20 seconds before and after using the toilet or touching any surface.")
-    elif age >= 65:
-        print("Please dry your hands with soap before continuing with any activity")
-    else:
-        print("Handwashing is not required for you.")
-else:
-    print("Invalid input. Please enter 'M' or 'F'.")
+
+import random
+
+class Battleship:
+    def __init__(self):
+        self.board = self.make_board()
+        self.ships = self.locate_ships()
+
+    @staticmethod
+    def make_board():
+        return [["."]*10 for _ in range(10)]
+
+    def locate_ships(self):
+        ship_list = []
+        for _ in range(5):
+            while True:
+                x = random.randint(0, 9)
+                y = random.randint(0, 9)
+                if (x, y) not in ship_list:
+                    ship_list.append((x, y))
+                    break
+        return ship_list
+
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(row))
+
+    def guess(self, x, y):
+        if (x, y) in self.ships:
+            self.board[x][y] = "X"
+            print("Hit")
+            self.ships.remove((x, y))
+            if not self.ships:
+                print("You win")
+        else:
+            self.board[x][y] = "O"
+            print("Miss")
+
+game = Battleship()
+game.print_board()
+
+while True:
+    x = int(input("Enter x: "))
+    y = int(input("Enter y: "))
+    game.guess(x, y)
+    game.print_board()
+
+
